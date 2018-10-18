bd6fc84b8367d2a755f0ab196e9730fcbc2ff468
diogobiz <diogo@codengage.com>
2018-10-18 14:22:49 -0300

    Set clearEmpties in employee

--- src/modules/Employee/Mutation.ts
+++ src/modules/Employee/Mutation.ts
@@ -1,7 +1,17 @@
 import { forwardTo } from 'prisma-binding'
+import { Context, clearEmpties } from '../../utils'
 
 export default {
   createEmployee: forwardTo('db'),
-  updateEmployee: forwardTo('db'),
+  updateEmployee: async (
+    parent,
+    { where: { id }, data: { ...args } },
+    ctx: Context,
+    info
+  ) =>
+    ctx.db.mutation.updateEmployee(
+      { where: { id }, data: clearEmpties(args) },
+      info
+    ),
   deleteEmployee: forwardTo('db')
 }
