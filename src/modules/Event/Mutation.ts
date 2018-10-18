5855c487e6bd5fb616b2ea23e63da08f39fe1765
diogobiz <diogo@codengage.com>
2018-10-18 08:58:27 -0300

    Set clearEmpties in event

--- src/modules/Event/Mutation.ts
+++ src/modules/Event/Mutation.ts
@@ -1,14 +1,21 @@
 import { forwardTo } from 'prisma-binding'
-import { Context, validateTwoDate } from '../../utils'
+import { Context, validateTwoDate, clearEmpties } from '../../utils'
 
 export default {
   createEvent: async (parent, { data }, ctx: Context, info) => {
-    !!data.startTime && !!data.endTime && validateTwoDate(data.startTime, data.endTime)
+    !!data.startTime &&
+      !!data.endTime &&
+      validateTwoDate(data.startTime, data.endTime)
     return ctx.db.mutation.createEvent({ data }, info)
   },
   updateEvent: async (parent, { where: { id }, data }, ctx: Context, info) => {
-    !!data.startTime && !!data.endTime && validateTwoDate(data.startTime, data.endTime)
-    return ctx.db.mutation.updateEvent({ where: { id }, data }, info)
+    !!data.startTime &&
+      !!data.endTime &&
+      validateTwoDate(data.startTime, data.endTime)
+    return ctx.db.mutation.updateEvent(
+      { where: { id }, data: clearEmpties(data) },
+      info
+    )
   },
   deleteEvent: forwardTo('db')
 }
--- src/modules/Vehicle/Query.ts
+++ src/modules/Vehicle/Query.ts
@@ -1,6 +1,6 @@
 import { forwardTo } from 'prisma-binding'
-import { Context, validateTwoDate } from '../../utils'
-import allVechile from './Vehicle'
+import { Context } from '../../utils'
+import * as moment from 'moment'
 
 export default {
   vehicle: forwardTo('db'),
@@ -8,23 +8,35 @@ export default {
   vehiclesConnection: forwardTo('db'),
   vehiclesAvailable: async (
     parent,
-    { startTime, endTime },
+    { startTime, endTime, filter },
     ctx: Context,
     info
   ) => {
-    validateTwoDate(startTime, endTime)
+    startTime = !startTime || !moment(startTime).isValid() ? null : startTime
+    endTime = !endTime || !moment(endTime).isValid() ? null : endTime
+
     return ctx.db.query.vehicles(
       {
         where: {
-          events_every: {
-            OR: [
-              { offer: { startTime_gt: endTime } },
-              { offer: { endTime_lt: startTime } }
-            ]
-          }
+          AND: [
+            {
+              events_every: {
+                OR: [
+                  { offer: { startTime_gt: endTime } },
+                  { offer: { endTime_lt: startTime } }
+                ]
+              }
+            },
+            {
+              OR: [
+                { name_contains: filter || '' },
+                { plate_contains: filter || '' }
+              ]
+            }
+          ]
         }
       },
-      allVechile
+      info
     )
   }
 }
